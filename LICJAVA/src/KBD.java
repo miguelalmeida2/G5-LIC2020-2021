19826760b6b00cb5353e2fe573c64354c9caa195
tp323 <tp323@users.noreply.github.com>
2021-01-09 20:28:07 +0000

    Update KBD.java

--- LICJAVA/src/KBD.java
+++ LICJAVA/src/KBD.java
@@ -1,7 +1,5 @@
 public class KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’*’ ou NONE.
 
-    private static final char NONE = 0;
-    private static char Key = NONE;
     private final static int KVAL_MASK = 0x10; // 0001 0000
     private final static int ACK_MASK = 0x80;  // 1000 0000
     private final static int KBD_MASK = 0x0F;  // 0000 1111
@@ -17,7 +15,7 @@ public class KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’
 
     // Retorna de imediato a tecla premida ou NONE se não há tecla premida.
     public static char getKey() {
-        char key = NONE;
+        char key = 0;
         if(HAL.isBit(KVAL_MASK)) {
             key = keyboard[KBD_MASK & HAL.readBits(KBD_MASK)];
             HAL.setBits(ACK_MASK);
@@ -29,11 +27,11 @@ public class KBD { // Ler teclas. Métodos retornam ‘0’..’9’,’#’,’
     // Retorna quando a tecla é premida ou NONE após decorrido ‘timeout’ milisegundos.
     public static char waitKey(long timeout) {
         timeout += System.currentTimeMillis();
-        Key = NONE;
+        char key = 0;
         do {
-            Key=getKey();
-            if (Key!=NONE) return Key;
-        } while (System.currentTimeMillis()<timeout);
-        return NONE;
+            key =getKey();
+            if (key != 0) return key;
+        } while (System.currentTimeMillis() < timeout);
+        return 0;
     }
 }
