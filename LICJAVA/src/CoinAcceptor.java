92f70c71bb5426d5b3e9bae4a2824723115841de
miguelalmeida2 <miguelalmeida991@protonmail.com>
2021-01-20 13:50:13 +0000

    Coin, File, Stats

---
+++ LICJAVA/src/CoinAcceptor.java
@@ -0,0 +1,15 @@
+public class CoinAcceptor {
+
+    private final static int COIN_MASK=0x40;
+    private final static int COIN_ACCEPT_MASK=0x40;
+
+    public static boolean checkForInsertedCoin(){
+        while (HAL.isBit(COIN_MASK)) {
+            HAL.setBits(COIN_ACCEPT_MASK);
+            HAL.clrBits(COIN_ACCEPT_MASK);
+            return true;
+        }
+        return false;
+    }
+}
+
